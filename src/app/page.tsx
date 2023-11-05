84b2f0c1bd34f9d58cd0966c3d377c5d7d897f4b
Bruno de A. Bisogni <bruno@sociodotabuleiro.fun>
2023-11-05 04:14:03 -0300

    finalização do estado de scrool do menu , a finalizar falta corrigir o js scritp que ter feito errado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,50 +4,50 @@ import { space } from 'postcss/lib/list';
 import Header from '../components/Header'
 
 
-const inter = Inter ({subsets: ['latin']}); 
+const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
-  
+
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
-      <Header/>
+      <Header />
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg-pb-12'>
-        <div className='absolute left-0 top-0 -z-10 flex flex-col h-[95vh] w-screen bg-yellow-500'>
-          
-        <Image
-        src='/6.jpg'
-        alt='banner image'
-        fill={true}
-        className='object-cover object-top lg:h-[95vh]'/>
-        
+          <div className='absolute left-0 top-0 -z-10 flex flex-col h-[95vh] w-screen bg-yellow-500'>
+
+            <Image
+              src='/6.jpg'
+              alt='banner image'
+              fill={true}
+              className='object-cover object-top lg:h-[95vh]' />
+
+          </div>
+
+          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl text-purple-800'>
+            Alugue Já
+          </h1>
+          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg lg:max-w-2xl  text-purple-800'>
+
+          </p>
+
+
         </div>
 
-        <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl text-purple-800'>
-        Alugue Já
-        </h1>
-        <p className='text-shadow-md max-w-xs text-xs md:max-w-lg lg:max-w-2xl  text-purple-800'>
-          
-        </p>
-        
-
-        </div>
-      
-      <div className='flex space-x-3 '>
-
-      <button className='
+        <div className='flex space-x-2 '>
+ 
+          <button className='
       bg-fuchsia-800
       text-yellow-500
       md:flex-xl; flex cursor-pointer items-center gap-2 rounded bg-fuchia-800
       px-5 py-1.5 text-sm font-semibold transition-opacity-75 md:px-8 md:py-2.5'>
-      Play</button>
+            Play</button>
 
-      <button className='
+          <button className='
       bg-purple-800
       text-yellow-500
       md:flex-xl; flex cursor-pointer items-center gap-2 rounded bg-fuchia-800
       px-5 py-1.5 text-sm font-semibold transition-opacity-75 md:px-8 md:py-2.5'>
-        Saiba mais</button>
-      </div>
+            Saiba mais</button>
+        </div>
       </main>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,37 @@
-import React from 'react'
+import React , { useEffect ,useState } from 'react'
 
-export default function Header () {
+export default function Header() {
+  const [isScrolled, setIsScrooled] = React.useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0 ) {
+        setIsScrooled(true);
+      } else{
+        setIsScrooled(false);
+      }
+    }; 
+
+    window.addEventListener('scroll' , handleScroll); 
+    return () => {
+      window.removeEventListener('scroll' , handleScroll);
+    };
+  } , []);
   return (
-    <header 
-    className='
-    fixed
-    top-0
-    z-50
-    flex
-    w-full
-    items-center
-    justify-between
-    px-4
-    py-4
-
-    lg:px-10
-    lg:py-6
-    '>
-      <div className='
-      flex 
-      items-center 
-      space-x-2
-
-      md:space-x-8
-      
-      '>
-      <img src='/logo_socio.svg' alt='logo socio' width={80} height={80}/>
-      <ul className='hidden md:flex md:space-x-4'>
-        <li>Cadastre seu acervo</li>
-        <li>Aluguel um jogo</li>
-        <li>Quem Somos</li>
-        <li>Contato</li>
-        <li>Nosso Blog</li>
-      </ul>
+    <header className='fixed top-0 z-50 flex w-full items-center justify-between transition-all px-4 py-4 lg:px-10 lg:py-6'>
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <img src='/logo_socio.svg' alt='logo socio' width={80} height={80} />
+        <ul className='hidden md:flex md:space-x-4'>
+          <li>Cadastre seu acervo</li>
+          <li>Aluguel um jogo</li>
+          <li>Quem Somos</li>
+          <li>Contato</li>
+          <li>Nosso Blog</li>
+        </ul>
+      </div>
+      <div className='flex items-center md:space-x-4'>
+        <p className='hidden cursor-not-allowed lg:inline'>UserAvatar</p>
+        <img src='https://rb.gy/g1pwyx' alt=''className='cursor-pointer rounded' />
       </div>
     </header>
   )
